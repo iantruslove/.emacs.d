3adb07c16b4c742163f8311e26e006d3d1275b0b
Ian Truslove <ian.truslove@gmail.com>
2013-11-17 17:44:44 -0700

    Added Ag plugin

--- custom/10projectile.el
+++ custom/10projectile.el
@@ -3,6 +3,9 @@
 (setq projectile-enable-caching t)
 (setq projectile-completion-system 'grizzl)
 
+;; Requiring ag seems to fix `C-c p A` functionality
+(require 'ag)
+
 ;; I have no idea why this matches C-c C-/, but it does and that's what I was after
 (global-set-key (kbd "C-c C-_") 'projectile-find-file)
 (global-set-key (kbd "C-c C-b") 'projectile-switch-to-buffer)
