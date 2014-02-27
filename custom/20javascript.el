d73abdee84b29f05d2b28fd58c93ac6fd6ad54be
Ian Truslove <ian.truslove@sonian.net>
2014-02-27 08:06:45 -0700

    Changing how js2-mode vars are configured

--- custom/20javascript.el
+++ custom/20javascript.el
@@ -1,9 +1,8 @@
+(setq js2-basic-offset 2)
+(setq js2-bounce-indent-p t)
+
 ;; Use js2-mode instead of javascript-mode
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
-
-(custom-set-variables
- '(js2-basic-offset 2)
- '(js2-bounce-indent-p t))
 
 ;; Enable js2-refactor
 (js2r-add-keybindings-with-prefix "C-c C-m")
