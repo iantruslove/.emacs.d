cf75c28f1d9561b0fd176207f6b352e9c8fe5a1c
Ian Truslove <ian.truslove@sonian.net>
2014-05-30 23:47:34 +0000

    Updated for latest clojure dev

--- init.el
+++ init.el
@@ -8,39 +8,43 @@
   (package-refresh-contents))
 
 ;; Add in your own as you wish:
-(defvar my-packages '(ac-nrepl
-                      auto-complete
-                      cl
+(defvar my-packages '(cl
                       cider
                       clojure-mode
                       clojure-test-mode
                       color-theme-sanityinc-tomorrow
+                      company ;; for cider autocompletion
                       dash
                       expand-region
                       ;; flx-ido
                       ;; flycheck
-                      flymake
-                      flymake-cursor
-                      flymake-jshint
-                      helm
-                      helm-ls-git
+                      ;;flymake
+                      ;;flymake-cursor
+                      ;;flymake-jshint
+                      ;; helm
+                      ;; helm-ls-git
                       ;; helm-projectile
                       highlight-symbol
+		      ido
                       js2-mode
                       js2-refactor
+		      magit
+		      paredit
                       pkg-info
                       ;; projectile
                       simp
+                      slamhound
                       ;;starter-kit
                       ;;starter-kit-lisp
                       ;;starter-kit-bindings
                       markdown-mode
                       rainbow-delimiters
                       ;; smart-window
-                      skewer-mode
-                      tree-mode
-                      yasnippet
-                      yas-jit
+                      ;;skewer-mode
+		      smex
+                      ;;tree-mode
+                      ;;yasnippet
+                      ;;yas-jit
                       ;; zenburn-theme
                       zencoding-mode
                       ;;emacs-pry
@@ -50,9 +54,60 @@
   (when (not (package-installed-p p))
     (package-install p)))
 
+;; Indentation
+(setq tab-width 2
+      indent-tabs-mode nil)
+(setq-default indent-tabs-mode nil)
+
+;; Suppress the startup screen
+(setq inhibit-startup-screen t)
+
 ;; (load-theme 'wombat t)
 ;; (load-theme 'zenburn t)
 (load-theme 'sanityinc-tomorrow-night t)
+
+;; Shorter answers to questions
+(defalias 'yes-or-no-p 'y-or-n-p)
+
+(global-set-key (kbd "RET") 'newline-and-indent)
+(global-set-key (kbd "C-;") 'comment-or-uncomment-region)
+(global-set-key (kbd "M-/") 'hippie-expand)
+(global-set-key (kbd "C-c q") 'join-line)
+
+;; Yes, ido please
+(ido-mode t)
+(setq ido-enable-flex-matching t ;; enable fuzzy matching
+      ido-use-virtual-buffers t)
+
+;; smex is ido for the minibuffer
+(setq smex-save-file (expand-file-name ".smex-items" user-emacs-directory))
+(smex-initialize)
+(global-set-key (kbd "M-x") 'smex)
+(global-set-key (kbd "M-X") 'smex-major-mode-commands)
+
+;; Shortcut for magit-status
+(global-set-key (kbd "C-c g") 'magit-status)
+
+;; Disable backup files
+(setq make-backup-files nil)
+
+;; Always reload files changed from underneath
+(global-auto-revert-mode t)
+
+;; Show line and col number
+(setq column-number-mode t)
+
+;; Short alias for query-replace-regexp
+(defalias 'qrr 'query-replace-regexp)
+
+;; Enable highlight-symbol-mode
+(add-hook 'prog-mode-hook (lambda () (highlight-symbol-mode)))
+(setq highlight-symbol-idle-delay 0.25) ;; 0.5 seconds
+(global-set-key (kbd "C-c h h") 'highlight-symbol-at-point)
+(global-set-key (kbd "C-c h r") 'highlight-symbol-remove-all)
+(global-set-key (kbd "C-c h n") 'highlight-symbol-next)
+(global-set-key (kbd "C-c h p") 'highlight-symbol-prev)
+(global-set-key (kbd "C-c h q r") 'highlight-symbol-query-replace)
 
 ;; Show matching paren
 (require 'paren)
@@ -72,6 +127,29 @@
 (add-hook 'emacs-lisp-mode-hook 'rainbow-delimiters-mode)
 (add-hook 'cider-repl-mode-hook 'rainbow-delimiters-mode)
 
+;; Fire up Paredit when in various clojure modes
+(add-hook 'clojure-mode-hook 'paredit-mode)
+(add-hook 'cider-repl-mode-hook 'paredit-mode)
+
+(add-hook 'clojure-mode-hook 'clojure-test-mode)
+(add-hook 'cider-mode-hook 'cider-turn-on-eldoc-mode)
+
+(add-hook 'paredit-mode-hook
+	  (lambda ()
+	    ;; For Joe
+	    (define-key paredit-mode-map (kbd "C-o C-r") 'paredit-forward-slurp-sexp)
+	    (define-key paredit-mode-map (kbd "C-o M-r") 'paredit-forward-barf-sexp)
+	    (define-key paredit-mode-map (kbd "C-o C-l") 'paredit-backward-slurp-sexp)
+	    (define-key paredit-mode-map (kbd "C-o M-l") 'paredit-backward-barf-sexp)
+
+	    ;; For Daniel
+	    (define-key paredit-mode-map (kbd "M-)") 'paredit-forward-slurp-sexp)
+	    (define-key paredit-mode-map (kbd "M-(") 'paredit-backward-slurp-sexp)
+
+	    ;; Since Daniel's slurp bindings squash paredit-wrap-round...
+	    (define-key paredit-mode-map (kbd "C-o C-w (") 'paredit-wrap-round)
+	                (define-key paredit-mode-map (kbd "C-o C-w [") 'paredit-wrap-square)))
+
 ;; Convenient keybinding to switch to REPL, given C-z is the tmux
 ;; prefix key
 (add-hook 'cider-mode-hook
@@ -79,6 +157,32 @@
             (define-key cider-mode-map (kbd "C-c z") 'cider-switch-to-relevant-repl-buffer)
             ;;(define-key cider-mode-map (kbd "C-c M-n") 'nrepl-set-ns)
             ))
+
+;; Make code purdy, stolen from larkin and ESK
+(defun untabify-buffer ()
+  (interactive)
+  (untabify (point-min) (point-max)))
+
+(defun indent-buffer ()
+  (interactive)
+  (indent-region (point-min) (point-max)))
+
+(defun cleanup-buffer ()
+  "Perform a bunch of operations on the whitespace content of a buffer."
+  (interactive)
+  (if (< (buffer-size) 100000)
+      (progn
+	(indent-buffer)
+	(untabify-buffer)
+	(delete-trailing-whitespace)
+	(message "Cleaned up buffer."))
+    (message "Didn't clean up buffer.")))
+
+(global-set-key (kbd "C-c n") 'cleanup-buffer)
+
+;; Unique buffer names
+(require 'uniquify)
+(setq uniquify-buffer-name-style 'forward)
 
 ;; >>> Configure Load Path <<<
 ;; From http://stackoverflow.com/a/1062314/594677
@@ -97,7 +201,6 @@
 
 (require 'transpose-frame)
 
-
 (let ((sonian-stuff "~/projects/sa-safe/.elisp/sonian.el"))
   (when (file-exists-p sonian-stuff)
     (message "Loading Sonian extras...")
@@ -106,51 +209,13 @@
     ;; the time
     (add-hook 'clojure-mode-hook 'whitespace-mode)))
 
-;; >>> ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-
-;; Configure simp - it's like Vim's Ctrl-P
-;; (require 'simp)
-;; (simp-project-define
-;;  '(:has (.git)
-;;         :ignore (tmp coverage log vendor .git public/system public/assets)))
-;; (simp-project-define
-;;  '(:has (.svn)
-;;         :ignore (.svn)))
-;; (global-set-key [(control p)] 'simp-project-find-file)
-
-                                        ;(projectile-global-mode)
-                                        ;(helm-mode 1)
-
-
 ;;; Load window management crap
-                                        ;(load "~/.emacs.d/emacsd-tile.el")
+;; (load "~/.emacs.d/emacsd-tile.el")
 
-;; Enable autp-complete - taken in part from http://www.emacswiki.org/emacs/init-auto-complete.el
-(require 'auto-complete)
-(global-auto-complete-mode t)           ;enable global-mode
-(setq ac-auto-start t)                  ;automatically start
+(menu-bar-mode -1)
 
-(setq ac-modes
-      '(emacs-lisp-mode lisp-interaction-mode lisp-mode scheme-mode
-                        c-mode cc-mode c++-mode
-                        clojure-mode java-mode
-                        perl-mode cperl-mode python-mode ruby-mode
-                        ecmascript-mode javascript-mode js2-mode php-mode css-mode
-                        makefile-mode sh-mode fortran-mode f90-mode ada-mode
-                        xml-mode sgml-mode
-                        haskell-mode literate-haskell-mode
-                        emms-tag-editor-mode
-                        asm-mode
-                        org-mode))
-
-
-;; Set up ac-nrepl, per https://github.com/clojure-emacs/ac-nrepl#installation
-(require 'ac-nrepl)
-(add-hook 'cider-repl-mode-hook 'ac-nrepl-setup)
-(add-hook 'cider-mode-hook 'ac-nrepl-setup)
-(eval-after-load "auto-complete"
-  '(add-to-list 'ac-modes 'cider-repl-mode))
+;; Enable global auto-complete with company-mode:
+(add-hook 'after-init-hook 'global-company-mode)
 
 ;; ClojureScript
 (add-to-list 'auto-mode-alist '("\.cljs$" . clojure-mode))
@@ -159,14 +224,14 @@
 ;;(autoload 'js2-mode "js2" nil t)
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
 
-(require 'flymake-jshint)
-(require 'flymake-cursor)
-(add-hook 'js2-mode-hook 'flymake-mode)
+;;(require 'flymake-jshint)
+;;(require 'flymake-cursor)
+;;(add-hook 'js2-mode-hook 'flymake-mode)
 
 ;; Load in skewer-mode
-(add-hook 'js2-mode-hook 'skewer-mode)
-(add-hook 'css-mode-hook 'skewer-css-mode)
-(add-hook 'html-mode-hook 'skewer-html-mode)
+;; (add-hook 'js2-mode-hook 'skewer-mode)
+;; (add-hook 'css-mode-hook 'skewer-css-mode)
+;; (add-hook 'html-mode-hook 'skewer-html-mode)
 
 ;; JS2-refactor mode
 (js2r-add-keybindings-with-prefix "C-c C-m")
@@ -183,9 +248,9 @@
  scroll-conservatively 100000
  scroll-preserve-screen-position 1)
 
-(electric-indent-mode +1)
+;;(electric-indent-mode +1)
 
-(auto-fill-mode -1)
+;;(auto-fill-mode -1)
 
 ;; Enable winner mode - C-c <left> to go back a window configuration
 (winner-mode)
@@ -193,13 +258,10 @@
 ;; Enable rainbow parens for all programming modes
 ;;(add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
 
-
 ;; Custom keybindings
 (global-set-key (kbd "M-@") 'er/expand-region) ; expand selected region
 
-
 ;; In-place scrolling
-
 (defun scroll-down-in-place (n)
   (interactive "p")
   (next-line n)
@@ -212,13 +274,13 @@
 
 
 ;; Enable helm mode
-(helm-mode t)
+;;(helm-mode t)
 
-(global-set-key (kbd "C-x C-_")
-                ;; I have no idea why this matches C-x C-/, but it does and that's what I was after
-                (lambda ()
-                  (interactive)
-                  (helm :sources '(helm-source-findutils) :buffer "*helm-findutils*")))
+;; (global-set-key (kbd "C-x C-_")
+;;                 ;; I have no idea why this matches C-x C-/, but it does and that's what I was after
+;;                 (lambda ()
+;;                   (interactive)
+;;                   (helm :sources '(helm-source-findutils) :buffer "*helm-findutils*")))
 
 
 (custom-set-variables
