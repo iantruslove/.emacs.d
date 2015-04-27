ad1713c7db0559800c92ac39d635ef4a7c04d9d2
Ian Truslove <ian.truslove@gmail.com>
2015-04-27 00:25:56 +0000

    Dear New Magit, it's all ok, I promise.

--- customizations/init-magit.el
+++ customizations/init-magit.el
@@ -4,7 +4,9 @@
  magit-save-some-buffers nil
  magit-process-popup-time 10
  magit-diff-refine-hunk nil
- magit-completing-read-function 'magit-ido-completing-read)
+ magit-completing-read-function 'magit-ido-completing-read
+ magit-auto-revert-mode t
+ magit-last-seen-setup-instructions "1.4.0")
 
 (global-set-key (kbd "C-c g") 'magit-status)
 
