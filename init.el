a5fd19df083cb2751ddcd9d5f5d0cf7187364ecc
Ian Truslove <ian.truslove@gmail.com>
2017-01-24 06:50:46 -0700

    Tweak smooth scroll

--- init.el
+++ init.el
@@ -77,7 +77,8 @@
 (use-package smooth-scrolling
   :ensure t
   :config
-  (smooth-scrolling-mode 1))
+  (smooth-scrolling-mode 1)
+  (setq smooth-scroll-margin 1))
 
 (use-package fill-column-indicator
   :ensure t)
