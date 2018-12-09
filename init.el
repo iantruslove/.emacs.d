665908b62f63157782426b73792fd66078dac08a
Ian Truslove <ian.truslove@gmail.com>
2018-12-09 09:33:21 -0700

    Fix racer's RUST_SRC_PATH

--- init.el
+++ init.el
@@ -735,6 +735,15 @@
   :requires rust-mode
   :init
   (setq company-tooltip-align-annotations t)
+
+  ;; Problems with racer having an incorrect RUST_SRC_PATH were fixed
+  ;; with this setting, and by installing the rust sources with
+  ;; `rustup component add rust-src`.
+  :init (setq racer-rust-src-path
+              (concat (string-trim
+                       (shell-command-to-string "rustc --print sysroot"))
+                      "/lib/rustlib/src/rust/src"))
+
   :config
   (add-hook 'rust-mode-hook #'racer-mode)
   (add-hook 'racer-mode-hook #'eldoc-mode)
