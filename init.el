8da4abda5ede58174d54814be79836b43ae09db6
Ian Truslove <ian.truslove@gmail.com>
2019-03-31 08:04:54 -0600

    Change when package-refresh is called on new inits

--- init.el
+++ init.el
@@ -17,8 +17,8 @@
 ;; Set up use-package
 
 (eval-when-compile
-  (package-refresh-contents)
   (unless (package-installed-p 'use-package)
+    (package-refresh-contents)
     (package-install 'use-package))
 
   (setq use-package-always-ensure t)
