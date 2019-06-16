e735a486c35c1922a087196fe57bc5df53197087
Ian Truslove <ian.truslove@gmail.com>
2019-06-15 19:47:23 -0600

    Comment out a bunch of session stuff

--- init.el
+++ init.el
@@ -177,41 +177,42 @@
       desktop-load-locked-desktop t)  ;; don't worry about loading it over a lock
 (desktop-save-mode 1)
 (setq-default history-length 1000)
-(savehist-mode t)
-(setq desktop-globals-to-save
-      ;; TODO: There's surely a bunch of cruft in here to clean up
-      (append '((comint-input-ring        . 50)
-                (compile-history          . 30)
-                desktop-missing-file-warning
-                (dired-regexp-history     . 20)
-                (extended-command-history . 30)
-                (face-name-history        . 20)
-                (file-name-history        . 100)
-                (grep-find-history        . 30)
-                (grep-history             . 30)
-                (ido-buffer-history       . 100)
-                (ido-last-directory-list  . 100)
-                (ido-work-directory-list  . 100)
-                (ido-work-file-list       . 100)
-                (magit-read-rev-history   . 50)
-                (minibuffer-history       . 50)
-                (org-clock-history        . 50)
-                (org-refile-history       . 50)
-                (org-tags-history         . 50)
-                (query-replace-history    . 60)
-                (read-expression-history  . 60)
-                (regexp-history           . 60)
-                (regexp-search-ring       . 20)
-                register-alist
-                (search-ring              . 20)
-                (shell-command-history    . 50)
-                tags-file-name
-                tags-table-list)))
 
-(use-package session
-  :config
-  (setq session-save-file (expand-file-name ".session" user-emacs-directory))
-  (add-hook 'after-init-hook 'session-initialize))
+(comment (savehist-mode t)
+         (setq desktop-globals-to-save
+               ;; TODO: There's surely a bunch of cruft in here to clean up
+               (append '((comint-input-ring        . 50)
+                         (compile-history          . 30)
+                         desktop-missing-file-warning
+                         (dired-regexp-history     . 20)
+                         (extended-command-history . 30)
+                         (face-name-history        . 20)
+                         (file-name-history        . 100)
+                         (grep-find-history        . 30)
+                         (grep-history             . 30)
+                         (ido-buffer-history       . 100)
+                         (ido-last-directory-list  . 100)
+                         (ido-work-directory-list  . 100)
+                         (ido-work-file-list       . 100)
+                         (magit-read-rev-history   . 50)
+                         (minibuffer-history       . 50)
+                         (org-clock-history        . 50)
+                         (org-refile-history       . 50)
+                         (org-tags-history         . 50)
+                         (query-replace-history    . 60)
+                         (read-expression-history  . 60)
+                         (regexp-history           . 60)
+                         (regexp-search-ring       . 20)
+                         register-alist
+                         (search-ring              . 20)
+                         (shell-command-history    . 50)
+                         tags-file-name
+                         tags-table-list))))
+
+(comment (use-package session
+           :config
+           (setq session-save-file (expand-file-name ".session" user-emacs-directory))
+           (add-hook 'after-init-hook 'session-initialize)))
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
