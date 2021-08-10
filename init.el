830f073e0c0409631ce150b415494a5bd9b3386c
Ian Truslove <ian.truslove@gmail.com>
2021-08-10 10:09:43 -0600

    Comment out marmalade. It's been SSL-borked for ages.

--- init.el
+++ init.el
@@ -6,7 +6,7 @@
 (setq package-enable-at-startup nil)
 
 (setq package-archives
-      '(("marmalade" . "https://marmalade-repo.org/packages/")
+      '(;;("marmalade" . "https://marmalade-repo.org/packages/")
         ("melpa-stable" . "https://stable.melpa.org/packages/")
         ("melpa" . "http://melpa.org/packages/")
         ("gnu" . "http://elpa.gnu.org/packages/")
